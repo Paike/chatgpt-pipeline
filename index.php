31f8979de49df1eb0c4d0172465feb8558e195d2
Patrick <Paike@users.noreply.github.com>
2024-05-30 12:08:35 +0200

    This code will generate a random number of iterations and output whether each iteration is even or odd.

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php for ($i = 0; $i < rand(1, 10); $i++) { echo 'Iteration ' . ($i+1) . '\n'; if ($i % 2 == 0) { echo 'Even iteration\n'; } else { echo 'Odd iteration\n'; } } ?>
+<?php for ($i = 0; $i < rand(1, 10); $i++) { echo 'Iteration ' . ($i+1) . '
+'; if ($i % 2 == 0) { echo 'Even iteration
+'; } else { echo 'Odd iteration
+'; } } ?>
