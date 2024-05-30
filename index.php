e09132337db9173a8cf2bf00845341675129f657
Patrick <Paike@users.noreply.github.com>
2024-05-30 12:08:19 +0200

    Added condition to check if iteration is even or odd

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php for ($i = 0; $i < rand(1, 10); $i++) { echo 'Iteration ' . ($i+1) . '\n'; } ?>
+<?php for ($i = 0; $i < rand(1, 10); $i++) { echo 'Iteration ' . ($i+1) . '\n'; if ($i % 2 == 0) { echo 'Even iteration\n'; } else { echo 'Odd iteration\n'; } } ?>
