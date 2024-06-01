14db75b50878bcf16d407d913d1de4729e5a64d8
Patrick <Paike@users.noreply.github.com>
2024-06-01 16:21:46 +0200

    Code updated with while loop for iteration, if-construct for checking product, and adding 2 to each number in the array.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,23 @@
 <?php
 $numbers = [3, 5, 7, 9, 11];
 
-// Using a for loop to iterate through the numbers array and display each number squared
-for($i = 0; $i < count($numbers); $i++){
+// Using a while loop to iterate through the numbers array and display each number squared
+$i = 0;
+while($i < count($numbers)){
     echo $numbers[$i] * $numbers[$i] . ' ';
+    $i++;
 }
 
-// Checking if sum of numbers is greater than 30
-$sum = array_sum($numbers);
-if($sum > 30){
-    echo 'Sum is greater than 30';
+// Checking if product of numbers is greater than 30
+$product = array_product($numbers);
+if($product > 30){
+    echo 'Product is greater than 30';
 } else {
-    echo 'Sum is not greater than 30';
+    echo 'Product is not greater than 30';
 }
 
-// Subtracting 2 from each number in the array and storing in a new array
-$subtractedNumbers = array_map(function($num) {
-    return $num - 2;
+// Adding 2 to each number in the array and storing in a new array
+$addedNumbers = array_map(function($num) {
+    return $num + 2;
 }, $numbers);
-echo implode(' ', $subtractedNumbers);
+echo implode(' ', $addedNumbers);
