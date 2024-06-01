7bfd13e7613a726ffba5d222f35e2196a7750b53
Patrick <Paike@users.noreply.github.com>
2024-06-01 15:19:52 +0200

    Code successfully rewritten to display iterations and their type (even or odd)

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 for ($i = 0; $i < rand(1, 10); $i++) {
-echo 'Iteration ' . ($i+1) . '\n';
+echo 'Iteration ' . ($i+1) . '\\n';
 if ($i % 2 == 0) {
-echo 'Even iteration\n';
+echo 'Even iteration\\n';
 } else {
-echo 'Odd iteration\n';
+echo 'Odd iteration\\n';
 }
 }
 ?>
