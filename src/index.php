728c47fcc5750e1be1db71f75373ec02ada35103
Patrick <Paike@users.noreply.github.com>
2024-06-01 16:14:29 +0200

    Updated code with while loops for iteration and if-construct for condition checking.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,31 @@
 <?php
 $numbers = [3, 5, 7, 9, 11];
 
-// Iterating through the new numbers array and displaying each number squared
-foreach($numbers as $number){
-    echo $number * $number . ' ';
+// Using a while loop to iterate through the numbers array and display each number squared
+$i = 0;
+while($i < count($numbers)){
+    echo $numbers[$i] * $numbers[$i] . ' ';
+    $i++;
 }
 
-// Calculating the sum of the numbers array
+// Checking if sum of numbers is greater than 30
 $sum = 0;
-foreach($numbers as $number){
-    $sum += $number;
+$i = 0;
+while($i < count($numbers)){
+    $sum += $numbers[$i];
+    $i++;
 }
-echo 'Sum: ' . $sum;
+if($sum > 30){
+    echo 'Sum is greater than 30';
+} else {
+    echo 'Sum is not greater than 30';
+}
 
-// Adding 3 to each number in the array and storing in a new array
-$addedNumbers = [];
-foreach($numbers as $number){
-    $addedNumbers[] = $number + 3;
+// Subtracting 2 from each number in the array and storing in a new array
+$subtractedNumbers = [];
+$i = 0;
+while($i < count($numbers)){
+    $subtractedNumbers[] = $numbers[$i] - 2;
+    $i++;
 }
-echo implode(' ', $addedNumbers);
+echo implode(' ', $subtractedNumbers);
