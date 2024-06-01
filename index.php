911c1e7e088c722ef408c714119b34e8e3916f4f
Patrick <Paike@users.noreply.github.com>
2024-06-01 15:07:04 +0200

    Added random number of iterations with even and odd messages

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-<?php for ($i = 0; $i < rand(1, 10); $i++) { echo 'Iteration ' . ($i+1) . '
-'; if ($i % 2 == 0) { echo 'Even iteration
-'; } else { echo 'Odd iteration
-'; } } ?>
+<?php for ($i = 0; $i < rand(1, 10); $i++) { echo 'Iteration ' . ($i+1) . '\n'; if ($i % 2 == 0) { echo 'Even iteration\n'; } else { echo 'Odd iteration\n'; } } ?>
