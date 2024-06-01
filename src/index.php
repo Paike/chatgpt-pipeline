f9152cf744d770f37cb3fdde02f07ab90943ddc1
Patrick <Paike@users.noreply.github.com>
2024-06-01 16:07:49 +0200

    Code includes examples of iterating through arrays, calculating sum, multiplying numbers, and using a while loop to display array elements.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,26 @@
 <?php
 $numbers = [1, 2, 3, 4, 5];
+
+// Iterating through the numbers array and displaying each number multiplied by 2
 foreach($numbers as $number){
     echo $number * 2 . ' ';
 }
 
+// Calculating the sum of the numbers array
 $sum = 0;
 foreach($numbers as $number){
     $sum += $number;
 }
 echo 'Sum: ' . $sum;
 
+// Multiplying each number in the array by 3 and storing in a new array
 $multipliedNumbers = [];
 foreach($numbers as $number){
     $multipliedNumbers[] = $number * 3;
 }
 echo implode(' ', $multipliedNumbers);
 
+// Using a while loop to display each number in the array
 $index = 0;
 $length = count($numbers);
 while($index < $length){
