e1f23fef5feb39bf1b7cb7f19427f676691a318e
Patrick <Paike@users.noreply.github.com>
2024-06-02 16:57:00 +0200

    New PHP code with different logic using for loop, sum comparison, and array subtraction

--- src/index.php
+++ src/index.php
@@ -1,23 +1,21 @@
 <?php
-$numbers = [2, 4, 6, 8, 10, 12];
+$numbers = [3, 5, 7, 9, 11, 13];
 
-// Using a while loop to iterate through the numbers array and displaying each number divided by 2
-$i = 0;
-while($i < count($numbers)) {
-    echo $numbers[$i] / 2 . ' ';
-    $i++;
+// Using a for loop to iterate through the numbers array and displaying each number multiplied by 2
+for($i = 0; $i < count($numbers); $i++) {
+    echo $numbers[$i] * 2 . ' ';
 }
 
-// Checking if product of numbers is less than 100
-$product = array_product($numbers);
-if($product < 100) {
-    echo 'Product is less than 100';
+// Checking if sum of numbers is greater than 50
+$sum = array_sum($numbers);
+if($sum > 50) {
+    echo 'Sum is greater than 50';
 } else {
-    echo 'Product is not less than 100';
+    echo 'Sum is not greater than 50';
 }
 
-// Adding 1 to each number in the array and storing in a new array
-$addedNumbers = array_map(function($num) {
-    return $num + 1;
+// Subtracting 1 from each number in the array and storing in a new array
+$subtractedNumbers = array_map(function($num) {
+    return $num - 1;
 }, $numbers);
-echo implode(' ', $addedNumbers);
+echo implode(' ', $subtractedNumbers);
