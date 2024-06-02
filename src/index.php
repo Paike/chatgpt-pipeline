3a657aab8c232af077f48f41e225517aec6c67a2
Patrick <Paike@users.noreply.github.com>
2024-06-02 16:47:07 +0200

    Code updated with while loop, product comparison, and addition operation.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,23 @@
 <?php
 $numbers = [2, 4, 6, 8, 10, 12];
 
-// Using a for loop to iterate through the numbers array and display each number multiplied by 3
-for($i = 0; $i < count($numbers); $i++){
-    echo $numbers[$i] * 3 . ' ';
+// Using a while loop to iterate through the numbers array and display each number multiplied by 2
+$i = 0;
+while($i < count($numbers)){
+    echo $numbers[$i] * 2 . ' ';
+    $i++;
 }
 
-// Checking if sum of numbers is greater than 40
-$sum = array_sum($numbers);
-if($sum > 40){
-    echo 'Sum is greater than 40';
+// Checking if product of numbers is less than 50
+$product = array_product($numbers);
+if($product < 50){
+    echo 'Product is less than 50';
 } else {
-    echo 'Sum is not greater than 40';
+    echo 'Product is not less than 50';
 }
 
-// Subtracting 1 from each number in the array and storing in a new array
-$subtractedNumbers = array_map(function($num) {
-    return $num - 1;
+// Adding 1 to each number in the array and storing in a new array
+$addedNumbers = array_map(function($num) {
+    return $num + 1;
 }, $numbers);
-echo implode(' ', $subtractedNumbers);
+echo implode(' ', $addedNumbers);
