3d7966714ef8c59c968028ed68e0d90e8de12fc3
Patrick <Paike@users.noreply.github.com>
2024-06-02 16:56:54 +0200

    Code has been replaced with a new code implementing while loop, different condition, and adding to numbers.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,23 @@
 <?php
-$numbers = [3, 5, 7, 9, 11, 13];
+$numbers = [2, 4, 6, 8, 10, 12];
 
-// Using a for loop to iterate through the numbers array and display each number multiplied by 3
-for($i=0; $i < count($numbers); $i++){
-    echo $numbers[$i] * 3 . ' ';
+// Using a while loop to iterate through the numbers array and displaying each number divided by 2
+$i = 0;
+while($i < count($numbers)) {
+    echo $numbers[$i] / 2 . ' ';
+    $i++;
 }
 
-// Checking if sum of numbers is greater than 50
-$sum = array_sum($numbers);
-if($sum > 50){
-    echo 'Sum is greater than 50';
+// Checking if product of numbers is less than 100
+$product = array_product($numbers);
+if($product < 100) {
+    echo 'Product is less than 100';
 } else {
-    echo 'Sum is not greater than 50';
+    echo 'Product is not less than 100';
 }
 
-// Subtracting 1 from each number in the array and storing in a new array
-$subtractedNumbers = array_map(function($num) {
-    return $num - 1;
+// Adding 1 to each number in the array and storing in a new array
+$addedNumbers = array_map(function($num) {
+    return $num + 1;
 }, $numbers);
-echo implode(' ', $subtractedNumbers);
+echo implode(' ', $addedNumbers);
